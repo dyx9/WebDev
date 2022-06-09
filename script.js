778af1d0c59558b88b338a68180a921579b8de82
dyx9 <747736317@qq.com>
2022-06-09 16:36:32 +0800

    clean up code and apply mobile first design

--- script.js
+++ script.js
@@ -20,38 +20,20 @@ function toggleMenu() {
 toggle.addEventListener("click", toggleMenu, false);
 
 
-/* Activate Submenu */
-function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
-  } else {
-    this.classList.add("submenu-active");
-  }
-}
  
-/* Event Listeners */
-for (let item of items) {
-    if (item.querySelector(".submenu")) {
-      item.addEventListener("click", toggleItem, false);
-      item.addEventListener("keypress", toggleItem, false);
-    }   
-}
 
-/* Close Submenu From Anywhere */
-function closeSubmenu(e) {
-    if (menu.querySelector(".submenu-active")) {
-      let isClickInside = menu
-        .querySelector(".submenu-active")
-        .contains(e.target);
+// /* Close Menu From Anywhere */
+// function closeMenu(e) {
+//     if (menu.classList.contains("active")) {
+//         let isClickInside = menu.contains(e.target);
+//         console.log(isClickInside);
+
+//         if (!isClickInside && menu.classList.contains("active")) {
+//             menu.classList.remove("active");
+//             toggle.querySelector("a").innerHTML = '<i class="fas fa-bars"></i>';
+//         }
+//     }
+// }
    
-      if (!isClickInside && menu.querySelector(".submenu-active")) {
-        menu.querySelector(".submenu-active").classList.remove("submenu-active");
-      }
-    }
-  }
-   
-  /* Event listener */
-  document.addEventListener("click", closeSubmenu, false);
+// /* Event listener */
+// document.addEventListener("click", closeMenu, false);
