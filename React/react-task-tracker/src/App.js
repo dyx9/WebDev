eac0bfb22ec495ba5e7775136696f28bd29d5141
dyx9 <747736317@qq.com>
2022-06-11 13:15:01 +0800

    react tutorial: event listener delete task

--- React/react-task-tracker/src/App.js
+++ React/react-task-tracker/src/App.js
@@ -1,9 +1,43 @@
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'task 1',
+        day: 'Feb 5th',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'task 2',
+        day: 'Feb 6th',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'task 3',
+        day: 'Feb 7th',
+        reminder: true,
+    }
+  ])
+
+  // delete task
+  const deleteTask = (id) => {
+    console.log('delete', id)
+    setTasks(tasks.filter( (task) => task.id !== id))
+  }
+
+
   return (
     <div className="container">
       <Header />
+      {tasks.length <= 0 ? "No tasks" :
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      }
     </div>
   );
 }
