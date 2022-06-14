af1e1eeff2d2412a69fe3619f46ebccc2f354bb4
dyx9 <747736317@qq.com>
2022-06-14 11:30:02 +0800

    react tutorial: add tasks

--- React/react-task-tracker/src/App.js
+++ React/react-task-tracker/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
 
@@ -25,18 +26,38 @@ function App() {
     }
   ])
 
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   // delete task
   const deleteTask = (id) => {
     console.log('delete', id)
     setTasks(tasks.filter( (task) => task.id !== id))
   }
 
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+      task.id === id ? { ...task, reminder: !task.reminder } : task)
+    )
+  }
+
 
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length <= 0 ? "No tasks" :
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks 
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder} 
+        />
       }
     </div>
   );
