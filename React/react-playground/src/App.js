87de9ee32f36a2b9402ec16e2a6eab507e4b19ac
dyx9 <747736317@qq.com>
2022-06-25 15:48:58 +0800

    React responsive nav bar

---
+++ React/react-playground/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import NavBar from './component/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
---
+++ React/react-playground/src/component/NavBar.js
@@ -0,0 +1,45 @@
+import './NavBar.css';
+import { MdClose, MdMenu } from 'react-icons/md';
+import { useState, useRef } from 'react';
+
+const NavBar = () => {
+    const menuRef = useRef(null)
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const toggle = () => {
+        setOpenMenu(pre => !pre)
+        updateClassName()
+    }
+
+    const updateClassName = () => {
+        if (menuRef.current.className == "menu") {
+            menuRef.current.className = 'menu active'
+        }
+        else{
+            menuRef.current.className = 'menu'
+        }
+
+    }
+
+  return (
+    <nav>
+        <ul className="menu" ref={menuRef}>
+            <li className="logo"><a href="#">Creative Mind Agency</a></li>
+            <li className="item"><a href="#">Home</a></li>
+            <li className="item"><a href="#">About</a></li>
+            <li className="item"><a href="#">Blog</a></li>
+            <li className="item"><a href="#">Contact</a></li>
+            <li className="item"><a href="#">Log In</a></li>
+            <li className="item"><a href="#">Sign Up</a></li>
+
+            <li className="toggle" onClick={toggle}>
+                {!openMenu && <a href="#"><MdMenu /></a>}
+                {openMenu && <a href="#"><MdClose /></a>}
+            </li>
+
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
