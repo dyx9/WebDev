281e21b50785a64d0f4d4dbd651c0a75a3d12a8a
dyx9 <747736317@qq.com>
2022-07-11 20:34:16 +0800

    Change Sidebar Open Layout

--- PureHTML/side bar/script.js
+++ PureHTML/side bar/script.js
@@ -1,19 +1,23 @@
 const toggle = document.querySelector(".toggle");
 const sidebar = document.querySelector(".sidebar");
+const content = document.querySelector(".content")
 
 function toggleMenu() {
     if (sidebar.classList.contains("active")) {
         sidebar.classList.remove("active");
+        content.classList.remove("push");
         
         // adds the sidebar (hamburger) icon
         toggle.querySelector("a").innerHTML = '<i class="fas fa-bars"></i>';
     }
     else {
         sidebar.classList.add("active");
+        content.classList.add("push");
          
         // adds the close (x) icon
         toggle.querySelector("a").innerHTML = '<i class="fas fa-times"></i>';
     }
+
 }
 
 toggle.addEventListener("click", toggleMenu, false);
